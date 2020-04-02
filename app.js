7756ed9360d3368412403877412c9f79180cbcef
ashleshmahule <ashleshmahule15@gmail.com>
2020-04-03 01:23:29 +0530

    added node

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+router.get('/Trie.js',function(req,res){
+  res.sendFile(__dirname + '/Trie.js');
+});
+
+router.get('/Node.js',function(req,res){
+  res.sendFile(__dirname + '/Node.js');
+});
+
+router.get('/main.css',function(req,res){
+  res.sendFile(__dirname + '/main.css');
+});
+
+router.get('/tree.css',function(req,res){
+  res.sendFile(__dirname + '/tree.css');
+});
+router.get('/p5.min.js',function(req,res){
+  res.sendFile(__dirname + '/p5.min.js');
+});
+
+
+app.use('/', router);
+app.listen(process.env.port || 5000);
+
+console.log('Running at Port 5000');
